68c378bc0f9848232ccaa1d813524c6a861badf8
lammaah <87265940+lammaah@users.noreply.github.com>
2022-01-10 23:33:55 +0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
 
 
 class Ledtester(MycroftSkill):
@@ -12,12 +13,21 @@ class Ledtester(MycroftSkill):
 
 	@intent_handler(IntentBuilder('DoorUnlock').require('lock').require('door'))
 	def handle_Door_Unlock(self, message):
-		if message.data['lock'].upper() == "UNLOCK":
+		if message.data['lock'].upper() == 'UNLOCK':
 			GPIO.output(27, GPIO.HIGH)
 			self.speak('Door is unlocked')
-		elif message.data['lock'].upper() == "LOCK":
+		elif message.data['lock'].upper() == 'LOCK':
 			GPIO.output(27, GPIO.LOW)
 			self.speak('Door is locked')
+
+	@intent_handler(IntentBuilder('BlindOpen').require('open').require('blind'))
+	def handle_Blind_Open(self, message):
+		if message.data['open'].upper() == 'OPEN':
+			GPIO.output(22, GPIO.HIGH)
+			self.speak('Blind is open')
+		elif message.data['lock'].upper() == 'CLOSE':
+			GPIO.output(22, GPIO.LOW)
+			self.speak('Blind is closed')
 								  
 
 def create_skill():
